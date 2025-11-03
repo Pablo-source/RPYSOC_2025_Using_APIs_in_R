e0f04952d097d2bfecdc9f54d3a75a8f72e6f280
Pablo-source <datavizvalencia@gmail.com>
2025-11-03 23:40:12 +0000

    Completed example using GET function

--- 01 Installing required libraries for APIs.R
+++ 01 Installing required libraries for APIs.R
@@ -2,7 +2,11 @@
 
 # 1. Installing {httr} and {curl} libraries 
 # 1.1 Installed previously on my terminal Installed libcurl4-openssl-dev
-# sudo apt-get install -y libcurl4-openssl-dev
+# required: sudo apt-get install -y libcurl4-openssl-dev
 install.packages('httr', keep_outputs = TRUE, repos='https://ftp.fau.de/cran/')
 install.packages('curl', keep_outputs = TRUE, repos='https://ftp.fau.de/cran/')
 
+# installing package xml2
+# required: sudo apt-get install -y libxml2-dev
+install.packages('xml2')
+library(xml2)
---
+++ 02 Making APIs requests.R
@@ -0,0 +1,20 @@
+# 02 Making APIs requests
+
+library(httr)
+library(jsonlite)
+
+res = GET("https://api.open-notify.org/astros.json")
+res
+
+res2 =GET("https://givefood.org.uk/api/2/foodbanks/")
+str(res2)
+
+# Data.gov Utility Rates API
+sample2 <- GET("http://api.data.gov/nrel/utility_rates/v3.json?api_key=sample_key&address=1600+Amphitheatre+Parkway,+Mountain+View,+CA")
+content(sample2)
+# Output: 
+> content(sample2)
+{html_document}
+<html class="no-js datagov" dir="ltr" lang="en">
+  [1] <head>\n<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">\n<t ...
+[2] <body class="archive category">\n<div class="page-landing-page layout-demo">\n< ...
