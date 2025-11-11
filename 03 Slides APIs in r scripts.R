ae7236436b8ff9c0fdd84de7bfedbfd970c2f5cb
Pablo-source <datavizvalencia@gmail.com>
2025-11-11 09:45:01 +0000

    renamed slides r scripts file

--- 03 Slides APIs in r scripts.R
+++ 03 Slides APIs in r scripts.R
@@ -26,6 +26,9 @@ req
 # 3. PRACTICAL EXAMPLE
 # Getting data from US National Weather Service using an API
 
+
+# 3.1 Making a request through an API
+
 # First we need to pass to the request() function the URL of the API provided 
 # by the US National Weather Service:
 # To obtain the grid forecast for a point location, use the /points endpoint to
@@ -44,4 +47,27 @@ National_weather <- request(NWS_base_url) %>%  # Append now further parts of the
   )  %>%  # Append further parts to the initial URL
   req_perform() # Perform a Request to get a response 
 National_weather
+
+# The API will return a JSON file (Javascript Object Notation). It is an structured data we can explore.
+
+
+# 3.2 Obtain the API response as a list 
+
+#  We can `display` the contents of the JASON file in a more `readable` way:
+# Then you have to identify from the API output query, which bits to use for your analysis.
+# For that we will use the `glimpse()` function from {dplyr} package
+
+library(httr2)
+
+NWS_base_url <- 'https://api.weather.gov' # Base URL 
+
+NWS_response <- request(NWS_base_url) %>%  # Append now further parts of the URL. Makes a GET() request to the API
+  req_url_path_append(
+    'points',
+    '38.8894,-77.0352'
+  )  %>%  # Append further parts to the initial URL
+  req_perform() # Perform a Request to get a response 
+NWS_response
+
+NWS_response %>% resp_body_json() # Display JSON file contents
 
