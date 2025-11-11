1778ca007faaf51aed43a07bd6553bd9a78f74b9
Pablo-source <datavizvalencia@gmail.com>
2025-11-11 09:12:51 +0000

    first weather data API query

--- 03 Slides R scripts.R
+++ 03 Slides R scripts.R
@@ -23,3 +23,25 @@ req <- request("http://example.com") |>
   req_auth_basic("username", "password")
 req
 
+# 3. PRACTICAL EXAMPLE
+# Getting data from US National Weather Service using an API
+
+# First we need to pass to the request() function the URL of the API provided 
+# by the US National Weather Service:
+# To obtain the grid forecast for a point location, use the /points endpoint to
+# retrieve the current grid forecast endpoint by coordinates:
+
+# Using website API query example: https://api.weather.gov/points/39.7456,-97.0892
+
+library(httr2)
+
+NWS_base_url <- 'https://api.weather.gov' # Base URL 
+
+National_weather <- request(NWS_base_url) %>%  # Append now further parts of the URL. Makes a GET() request to the API
+  req_url_path_append(
+    'points',
+    '38.8894,-77.0352'
+  )  %>%  # Append further parts to the initial URL
+  req_perform() # Perform a Request to get a response 
+National_weather
+
