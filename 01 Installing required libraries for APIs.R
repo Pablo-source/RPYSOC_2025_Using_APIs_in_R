e025bfe13ba215231922eb1d165491278995e055
Pablo-source <datavizvalencia@gmail.com>
2025-12-23 09:27:41 +0000

    installing climaet library

--- 01 Installing required libraries for APIs.R
+++ 01 Installing required libraries for APIs.R
@@ -6,11 +6,17 @@
 install.packages('httr', keep_outputs = TRUE, repos='https://ftp.fau.de/cran/')
 install.packages('curl', keep_outputs = TRUE, repos='https://ftp.fau.de/cran/')
 
-# installing package xml2
+# 2. installing package xml2
 # required: sudo apt-get install -y libxml2-dev
 install.packages('xml2')
 library(xml2)
 
 # 3.Installing {httr2} library
+# https://httr2.r-lib.org/
 install.packages('httr2',dependencies = TRUE)
+library(httr2)
+
+# 4. Installing {climaemet} library
+# https://github.com/rOpenSpain/climaemet
+install.packages('climaemet',dependencies = TRUE)
 library(httr2)
