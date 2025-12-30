5d494040d5bf720d7cbcd4211d6fd3cb99082175
Pablo-source <datavizvalencia@gmail.com>
2025-12-30 08:33:47 +0000

    started working on daily data script

--- 05 AEMET daily data API.R
+++ 05 AEMET daily data API.R
@@ -3,3 +3,6 @@
 ## AIM
 # Create charts using ggplot in R displaying MIN/MAX DAILY temperatures over last 10 years using AEMET OpenDATA API for these cities: Valencia, Sevilla, Barcelona and Madrid, to display effect of extreme weather in Spain.
 # Using API provided by AEMET: State Meteorological Agency – AEMET – Spanish Government
+
+# Obtain daily temperatures for these Spanish cities: Valencia, Sevilla, Barcelona, Madrid, for the 
+# last 20 years if available.
